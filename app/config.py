a6635fc8e740ddfbcb1ff26adcfc2765972c09eb
Zirondev <128257929+ZironDev@users.noreply.github.com>
2026-07-04 22:43:28 +0530

    Update config.py

--- app/config.py
+++ app/config.py
@@ -5,12 +5,12 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Settings:
-    HOST = os.getenv('LAVALINK_HOST', '0.0.0.0')
-    PORT = int(os.getenv('LAVALINK_PORT', '2333'))
-    PASSWORD = os.getenv('LAVALINK_PASSWORD', 'youshallnotpass')
-    LAVALINK_API_VERSION = 4
-    LAVALINK_SEMVER = '4.0.8'
-    LAVALINK_BUILD = 'zironlink-1.0.0'
+    HOST = os.getenv('ZIRONLINK_HOST', '0.0.0.0')
+    PORT = int(os.getenv('ZIRONLINK_PORT', '2333'))
+    PASSWORD = os.getenv('ZIRONLINK_PASSWORD', 'youshallnotpass')
+    ZIRONLINK_API_VERSION = 4
+    ZIRONLINK_SEMVER = '4.0.8'
+    ZIRONLINK_BUILD = 'zironlink-1.0.0'
     JVM = 'n/a (python)'
     SOURCE_MANAGERS = ['youtube', 'soundcloud', 'http']
     FILTERS = ['volume', 'bassboost', 'lofi', 'slowmo']
@@ -19,6 +19,6 @@ class Settings:
     PLAYER_UPDATE_INTERVAL_SECONDS = 5
     FFMPEG_PATH = os.getenv('FFMPEG_PATH', 'ffmpeg')
     DEBUG = os.getenv('ZIRONLINK_DEBUG', 'false').lower() in ('1', 'true', 'yes')
-    QUEUE_LIMIT_PER_GUILD = int(os.getenv('LAVALINK_QUEUE_LIMIT', '100'))
+    QUEUE_LIMIT_PER_GUILD = int(os.getenv('ZIRONLINK_QUEUE_LIMIT', '100'))
     DISCORD_BOT_TOKEN = os.getenv('DISCORD_BOT_TOKEN', '')
 settings = Settings()
